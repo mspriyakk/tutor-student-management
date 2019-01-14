e90ee63e3083dd351aa509e7ebdc2e10c7a85303
PKrishnakumar <PKrishnakumar@webmd.net>
2019-01-14 14:46:38 -0500

    rebuild after change homepage url in the package json

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14509824746a080d2bcba178948820af.js"
+  "/tutor-student-management/build/precache-manifest.0f557b589919b4996ec9cff53f968cf3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tutor-student-management/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
