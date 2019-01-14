cc3c6a4f6aa3988268c5f242c27da9b0b1883a3b
PKrishnakumar <PKrishnakumar@webmd.net>
2019-01-14 15:08:36 -0500

    updated homepage path to build folder

---
+++ build/precache-manifest.cd16d87859c0a19316599ec3d9fdfb1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c41542e07648518c6c6e",
+    "url": "./build/static/js/runtime~main.c41542e0.js"
+  },
+  {
+    "revision": "7d8a62ca4ddcf64d45ba",
+    "url": "./build/static/js/main.7d8a62ca.chunk.js"
+  },
+  {
+    "revision": "8562945b9b82aeffe44a",
+    "url": "./build/static/js/1.8562945b.chunk.js"
+  },
+  {
+    "revision": "59b4035f15edfe4e777e8364bb1ef370",
+    "url": "./build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7c2c602abe01cbbbdb07231ac9c6dc7d.js"
+  "./build/precache-manifest.cd16d87859c0a19316599ec3d9fdfb1a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("./build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
