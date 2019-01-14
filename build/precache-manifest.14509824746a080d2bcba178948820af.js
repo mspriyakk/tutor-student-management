724961d9266da6c3586c378da1aaedafcbcb9797
PKrishnakumar <PKrishnakumar@webmd.net>
2019-01-14 14:42:06 -0500

    deployment files from npm run build

---
+++ build/precache-manifest.14509824746a080d2bcba178948820af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d8a62ca4ddcf64d45ba",
+    "url": "/static/js/main.7d8a62ca.chunk.js"
+  },
+  {
+    "revision": "8562945b9b82aeffe44a",
+    "url": "/static/js/1.8562945b.chunk.js"
+  },
+  {
+    "revision": "709d74b24679f1dc1b6bda506d6a38a5",
+    "url": "/index.html"
+  }
+];
